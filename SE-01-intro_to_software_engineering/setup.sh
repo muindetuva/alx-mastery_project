447a00ad89b493ad8dcdf353ccf2de1357b45c04
Alfred Tuva <muindetuva@gmail.com>
2026-07-13 10:32:02 +0300

    add setup

---
+++ SE-01-intro_to_software_engineering/setup.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+PROJECT_NAME="$1"
+
+if [ -z "$PROJECT_NAME" ]; then
+  echo "Usage: ./setup.sh <project-name>"
+  exit 1
+fi
+
+if [ -e "$PROJECT_NAME" ]; then
+  echo "Error: project folder '$PROJECT_NAME' already exists."
+  exit 1
+fi
+
+echo "Creating workspace '$PROJECT_NAME'..."
+
+mkdir "$PROJECT_NAME"
+mkdir "$PROJECT_NAME/src" "$PROJECT_NAME/docs" "$PROJECT_NAME/tests" "$PROJECT_NAME/data"
+
+cat > "$PROJECT_NAME/README.md" <<README
+# $PROJECT_NAME
+
+Created on: $(date +"%Y-%m-%d")
+
+## Description
+
+TODO: Add a short description of this project.
+
+## Usage
+
+TODO: Add usage instructions.
+
+## Contributing
+
+TODO: Add contribution guidelines.
+README
+
+chmod +x "$0"
+
+cd "$PROJECT_NAME" || exit 1
+git init > /dev/null
